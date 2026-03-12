037d5eea57cc4440eb9640afc411f3e61121d645
Kautuk <kautukpandey2004@gmail.com>
2026-03-12 17:30:50 +0530

    Add authentication middleware and protect application routes

---
+++ Backend/server/routes/application.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    addApplication,
+    getApplications,
+    getApplicationById,
+    updateApplication,
+    deleteApplication
+} = require("../controllers/application.controller");
+
+const protect = require("../middleware/authMiddleware");
+
+
+router.post("/", protect, addApplication);
+router.get("/", protect, getApplications);
+router.get("/:id", protect, getApplicationById);
+router.put("/:id", protect, updateApplication);
+router.delete("/:id", protect, deleteApplication);
+
+module.exports = router;
